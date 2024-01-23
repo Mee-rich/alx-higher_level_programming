00442520206fa7d703d9412f5bef7f5c28332f8a
mee-rich <jamessuyi@gmail.com>
2024-01-23 23:32:22 +0100

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" Define a class Square """
+
+class Square:
+    """ Represents square. """
+
+    def ___init__(self, size):
+        """Initialize a new Square
+
+        Args:
+            size (int): Size of the new square.
+        """
+        self.__size = size
