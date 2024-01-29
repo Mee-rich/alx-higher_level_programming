6f41bb05df71e7d59cc2c6282d2f786bc6d1fb87
mee-rich <jamessuyi@gmail.com>
2024-01-29 22:51:02 +0100

    working

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" This module defines a rectangle"""
+
+class Rectangle:
+    """ A rectangle based on 0-rectangle module"""
+
+    def __init__(self, width=0, height=0):
+        """Initializing this rectangle
+        Args:
+            width: width
+            height: height
+        Raises: 
+            TypeError: If size is not integer
+            ValueError: If size is less than zero
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ field width is private"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """setter method to change width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0 :
+            raise TypeError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """field height is private"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter method to change height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise TypeError("height must be >= 0")
+        self.__height = value
