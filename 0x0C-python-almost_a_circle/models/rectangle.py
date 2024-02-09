3965a16afe807a23f055be155610809797775cac
mee-rich <jamessuyi@gmail.com>
2024-02-09 09:56:20 +0100

    area done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,7 +10,7 @@ class Rectangle(Base):
         width (int): width of rectangle.
         height (int): height of rectangle.
         x (int): x.
-        hy (int): y.
+        y (int): y.
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -28,3 +28,119 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    def __str__(self):
+       """Prints rectangle"""
+       return ("[Rectangle] ({}) {:d}/{:d} - {:}/{:d}".format(self.id, self.__x, self.__y, self.__width, self.__height))
+    
+    @property
+    def width(self):
+        """Width retriever.
+
+        Returns:
+            int: width of rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Property setter for width of rectangle.
+        
+        Args:
+            value (int): width of rectangle.
+
+        Raises:
+            TypeError: if width is not an integer.
+            valueError: if width is less than or equal to zero.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property 
+    def height(self):
+        """Height retriever.
+
+        Returns:
+            int: width of rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Property setter for height of rectangle.
+
+        Args:
+            value(int): height of rectangle.
+        
+        Raises:
+            TypeError: if height is not an integer.
+            ValueError: if height is less than or equal to zero.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """x retriever.
+
+        Returns:
+            int: x.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """property setter for x.
+
+        Args:
+            value (int): x.
+
+        Raises:
+            TypeError: if x is not an integer
+            ValueError: if x is less than zero.
+        """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Retriever for y.
+
+        Returns:
+            int: y.
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """property setter for y.
+
+        Args:
+            value (int): x.
+
+        Raises:
+            TypeError: if y is not an integer
+            ValueError: if y is less than zero.
+        """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """calculates the area of a rectangle.
+
+        Returns:
+            int: area.
+        """
+        return (self.__width * self.height)
