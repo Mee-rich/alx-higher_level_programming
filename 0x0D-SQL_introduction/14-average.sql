2e7a29736be57e0310f500d6a53a04edd4570287
mee-rich <jamessuyi@gmail.com>
2024-03-12 17:49:19 +0100

    see-quel

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,19 @@
+--  computes the score average of all records in the
+-- table second_table
+SELECT AVG(score) AS average FROM second_table;--  computes the score average of all records in the
+-- table second_table
+SELECT AVG(score) AS average FROM second_table;--  computes the score average of all records in the
+-- table second_table
+SELECT AVG(score) AS average FROM second_table;--  computes the score average of all records in the
+-- table second_table
+SELECT AVG(score) AS average FROM second_table;--  computes the score average of all records in the
+-- table second_table
+SELECT AVG(score) AS average FROM second_table;--  computes the score average of all records in the
+-- table second_table
+SELECT AVG(score) AS average FROM second_table;--  computes the score average of all records in the
+-- table second_table
+SELECT AVG(score) AS average FROM second_table;--  computes the score average of all records in the
+-- table second_table
+SELECT AVG(score) AS average FROM second_table;--  computes the score average of all records in the
+-- table second_table
+SELECT AVG(score) AS average FROM second_table;
