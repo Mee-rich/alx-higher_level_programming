ac28ab65853bb4792abcf1e72d5375c551ffa9d2
mee-rich <jamessuyi@gmail.com>
2024-04-09 06:07:43 +0100

    done

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Prints a square
+
+if (isNaN(process.argv[2])) {
+	console.log('Missing size');
+}
+else {
+	for (let j = 0; j < parseInt(process.argv[2]; j++) {
+		console.log('X'.repeat(parseInt(process.argv[2])));
+	}
+}
